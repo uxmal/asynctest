de07650183f61e9d16e5271f926dece71f0fcd1c
John Källén <johnkal@gmail.com>
2020-01-30 13:56:51 +0100

    Add a SimpleCopier

--- AsyncCode.cs
+++ AsyncCode.cs
@@ -1,65 +1,82 @@
 using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Test
+namespace AsyncTest
 {
-        public abstract class Copier
+    public abstract class Copier
+    {
+        public const int BlockSize = 8192;
+
+        public abstract void CopyBlockFromStream(Stream source, Stream destination);
+    }
+
+    public class NullCopier : Copier
+    {
+        public override void CopyBlockFromStream(Stream source, Stream destination)
         {
-            public abstract void CopyBlockFromStream(Stream source, Stream destination);
+            var buf = new byte[BlockSize];
+            var nRead = source.Read(buf, 0, buf.Length);
+            destination.Write(buf, 0, nRead);
         }
+    }
 
-        public class NullCopier : Copier
+    public class SimpleCopier : Copier
+    {
+        public override void CopyBlockFromStream(Stream source, Stream destination)
         {
-            public override void CopyBlockFromStream(Stream source, Stream destination)
-            {
-            }
+            var buf = new byte[BlockSize];
+            var nRead = source.Read(buf, 0, buf.Length);
+            destination.Write(buf, 0, nRead);
         }
+    }
+    public class MonitoredCopier : Copier
+    {
 
-        public class MonitoredCopier : Copier
+        private object monitor = new object();
+        // System.Threading.Monitor.Enter(monitor);
+        // System.Threading.Monitor.Exit(monitor)
+
+        public override void CopyBlockFromStream(Stream source, Stream destination)
         {
-            private object monitor = new object();
-            private const int BlockSize = 8192;
-
-            public override void CopyBlockFromStream(Stream source, Stream destination)
+            lock (monitor)
             {
-                lock (monitor)
-                {
-                    var buf = new byte[BlockSize];
-                    var nRead = source.Read(buf, 0, buf.Length);
-                    destination.Write(buf, 0, nRead);
-                }
-            }
-        }
-
-        public class UserCode
-        {
-            /// <summary>
-            /// The old code that we cannot really change.
-            /// </summary>
-            public void LegacyCopyBlock(Copier copier)
-            {
-                var buffer = GenerateRandomData();
-                var src = new MemoryStream(buffer);
-                var dst = new MemoryStream();
-                copier.CopyBlockFromStream(src, dst);
-            }
-
-            /// <summary>
-            /// New and exciting async code.
-            /// </summary>
-            public Task CopyBlockAsync(Copier copier)
-            {
-                //$TODO!
-                throw new NotImplementedException();
-            }
-
-            private static byte[] GenerateRandomData()
-            {
-                var random = new Random(4711);
-                var buf = new byte[random.Next(10000)];
-                random.NextBytes(buf);
-                return buf;
+                var buf = new byte[BlockSize];
+                var nRead = source.Read(buf, 0, buf.Length);
+                destination.Write(buf, 0, nRead);
             }
         }
+    }
+
+    public class UserCode
+    {
+        /// <summary>
+        /// The old code that we cannot really change.
+        /// </summary>
+        public void LegacyCopyBlock(Copier copier)
+        {
+            var buffer = GenerateRandomData();
+            var src = new MemoryStream(buffer);
+            var dst = new MemoryStream();
+            copier.CopyBlockFromStream(src, dst);
+        }
+
+        /// <summary>
+        /// New and exciting async code.
+        /// </summary>
+        public Task CopyBlockAsync(Copier copier)
+        {
+            //$TODO!
+            throw new NotImplementedException();
+        }
+
+        private static byte[] GenerateRandomData()
+        {
+            var random = new Random(4711);
+            var buf = new byte[random.Next(10000)];
+            random.NextBytes(buf);
+            return buf;
+        }
+    }
 }
